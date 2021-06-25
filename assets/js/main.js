dddd5e3626ef8855e234dd6960a3b2811516f007
jkijak <jakub.kijak@blackrock.com>
2021-06-25 21:07:14 +0100

    icons

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,28 +26,34 @@
 
   // Touch mode.
   if (browser.mobile) {
-    // var $wrapper;
-    // // Create wrapper.
-    // $body.wrapInner('<div id="wrapper" />');
-    // $wrapper = $('#wrapper');
-    // // Hack: iOS vh bug.
-    // if (browser.os == 'ios')
-    //   $wrapper.css('margin-top', -25).css('padding-bottom', 25);
-    // // Pass scroll event to window.
-    // $wrapper.on('scroll', function () {
-    //   $window.trigger('scroll');
-    // });
-    // // Scrolly.
-    // $window.on('load.hl_scrolly', function () {
-    //   $('.scrolly').scrolly({
-    //     speed: 1500,
-    //     parent: $wrapper,
-    //     pollOnce: true,
-    //   });
-    //   $window.off('load.hl_scrolly');
-    // });
-    // // Enable touch mode.
-    // $html.addClass('is-touch');
+    var $wrapper;
+
+    // Create wrapper.
+    $body.wrapInner('<div id="wrapper" />');
+    $wrapper = $('#wrapper');
+
+    // Hack: iOS vh bug.
+    if (browser.os == 'ios')
+      $wrapper.css('margin-top', -25).css('padding-bottom', 25);
+
+    // Pass scroll event to window.
+    $wrapper.on('scroll', function () {
+      $window.trigger('scroll');
+    });
+
+    // Scrolly.
+    $window.on('load.hl_scrolly', function () {
+      $('.scrolly').scrolly({
+        speed: 1500,
+        parent: $wrapper,
+        pollOnce: true,
+      });
+
+      $window.off('load.hl_scrolly');
+    });
+
+    // Enable touch mode.
+    $html.addClass('is-touch');
   } else {
     // Scrolly.
     $('.scrolly').scrolly({
@@ -61,31 +67,31 @@
     $headerContainer = $header.find('.container');
 
   // Make title fixed.
-  if (!browser.mobile) {
-    $window.on('load.hl_headerTitle', function () {
-      breakpoints.on('>medium', function () {
-        $headerTitle
-          .css('position', 'fixed')
-          .css('height', 'auto')
-          .css('top', '50%')
-          .css('left', '0')
-          .css('width', '100%')
-          .css('margin-top', $headerTitle.outerHeight() / -2);
-      });
-
-      breakpoints.on('<=medium', function () {
-        $headerTitle
-          .css('position', '')
-          .css('height', '')
-          .css('top', '')
-          .css('left', '')
-          .css('width', '')
-          .css('margin-top', '');
-      });
-
-      $window.off('load.hl_headerTitle');
+  // if (!browser.mobile) {
+  $window.on('load.hl_headerTitle', function () {
+    breakpoints.on('>medium', function () {
+      $headerTitle
+        .css('position', 'fixed')
+        .css('height', 'auto')
+        .css('top', '50%')
+        .css('left', '0')
+        .css('width', '100%')
+        .css('margin-top', $headerTitle.outerHeight() / -2);
     });
-  }
+
+    breakpoints.on('<=medium', function () {
+      $headerTitle
+        .css('position', '')
+        .css('height', '')
+        .css('top', '')
+        .css('left', '')
+        .css('width', '')
+        .css('margin-top', '');
+    });
+
+    $window.off('load.hl_headerTitle');
+  });
+  // }
 
   // Scrollex.
   breakpoints.on('>small', function () {
