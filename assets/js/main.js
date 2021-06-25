a0eeaf4d80809444207c49b05abe2ee4ca23bbf0
jkijak <jakub.kijak@blackrock.com>
2021-06-25 21:04:02 +0100

    icons

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,31 +29,31 @@
     var $wrapper;
 
     // Create wrapper.
-    // $body.wrapInner('<div id="wrapper" />');
-    // $wrapper = $('#wrapper');
+    $body.wrapInner('<div id="wrapper" />');
+    $wrapper = $('#wrapper');
 
     // Hack: iOS vh bug.
     if (browser.os == 'ios')
       $wrapper.css('margin-top', -25).css('padding-bottom', 25);
 
     // Pass scroll event to window.
-    // $wrapper.on('scroll', function () {
-    //   $window.trigger('scroll');
-    // });
+    $wrapper.on('scroll', function () {
+      $window.trigger('scroll');
+    });
 
     // Scrolly.
-    // $window.on('load.hl_scrolly', function () {
-    //   $('.scrolly').scrolly({
-    //     speed: 1500,
-    //     parent: $wrapper,
-    //     pollOnce: true,
-    //   });
+    $window.on('load.hl_scrolly', function () {
+      $('.scrolly').scrolly({
+        speed: 1500,
+        parent: $wrapper,
+        pollOnce: true,
+      });
 
-    //   $window.off('load.hl_scrolly');
-    // });
+      $window.off('load.hl_scrolly');
+    });
 
     // Enable touch mode.
-    $html.addClass('is-touch');
+    // $html.addClass('is-touch');
   } else {
     // Scrolly.
     $('.scrolly').scrolly({
